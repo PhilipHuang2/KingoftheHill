1ddfa33a5929ef01d7bef5dbb7b8159875b543d7
Philip Huang <philiphuang2@gmail.com>
2019-12-27 21:49:38 -0800

    Added Legend Button and Legend Image

--- app/src/main/java/com/example/kingofthehill/MainActivity.java
+++ app/src/main/java/com/example/kingofthehill/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.kingofthehill;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button legendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        legendButton = findViewById(R.id.legendButton);
+        legendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLegend();
+            }
+        });
+    }
+
+    public void openLegend()
+    {
+        Intent intent = new Intent(this, LegendActivity.class);
+        startActivity(intent);
     }
 }
